6775b5b42734b5c37d5278d0a6a6b835f8da737f
Marina Fabienne Pereira <pereiramarinafabienne@gmail.com>
2026-02-25 22:55:34 +0100

    connexion et inscriprtion

--- src/main/java/org/example/chatapp/HelloController.java
+++ src/main/java/org/example/chatapp/HelloController.java
@@ -1,14 +1,53 @@
 package org.example.chatapp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private PasswordField password_input;
+
+    @FXML
+    private TextField username_input;
+
+    @FXML
+    void login(ActionEvent event) {
+        String username = username_input.getText();
+        String password = password_input.getText();
+
+        if (username.isEmpty() || password.isEmpty()) {
+            System.out.println("Veuillez remplir tous les champs !");
+            return;
+        }
+
+
+        System.out.println("Connexion réussie pour : " + username);
+
+    }
+
+
+    @FXML
+    void goToRegister(MouseEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("inscription-view.fxml"));
+            AnchorPane root = loader.load();
+            Stage stage = (Stage) username_input.getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
